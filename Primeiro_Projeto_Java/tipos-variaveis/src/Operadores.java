b03594010e8c0d1e7291254d605e6e320fe29c27
GabrielPyrrho <pyrrhogabriel17@gmail.com>
2024-05-22 18:43:38 -0300

    Continuação do Estudo

---
+++ Primeiro_Projeto_Java/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        
+        //x repetição
+
+        numero ++;
+
+        System.out.println(numero);
+        
+        System.out.println(numero ++);
+    }
+
+}
